7c8d8c5b9ae424e29909aae8db5f3b1f20cd73af
Talia Kaplanian <tkaplani@redhat.com>
2020-07-01 16:11:41 -0400

    Port = 5000

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
